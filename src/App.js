04e400f96b4d17d8e121f7565f15d1cd206a4f47
jiangdongjiu <jiangdongjiu5@gmail.com>
2021-02-28 12:34:30 -0600

    change html title remove placeholder and unnecessary styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
         <h1>Movie Rater</h1>
+      </header>
     </div>
   );
 }
