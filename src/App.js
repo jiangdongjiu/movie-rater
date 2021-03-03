50450af3f4940e665a6b7745836ef4fe9508f662
jiangdongjiu <jiangdongjiu5@gmail.com>
2021-03-02 22:53:57 -0600

    use hook to fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json', // it means it will send the get requrest and receive json data
+        'Authorization': 'Token e9ddc5e91e1c82e33ca20a807f54238b766ada75' // need to be dynamic
+      }
+    })
+    .then( resp => resp.json()) // convert it to json
+    .then( resp => setMovies(resp)) // set movies
+    .catch( error => console.log(error))
+  }, []) //empty means it will run when app.js component did mount(run)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +25,11 @@ function App() {
 
       </header>
       <div className="Layout">
-        <div>Movie List: </div>
+        <div>Movie List:
+          { movies.map( movie => {
+            return <h2 key={movie.id}>{movie.title}</h2>;
+          })}
+        </div>
         <div>Movie Detail: </div>
       </div>
     </div>
