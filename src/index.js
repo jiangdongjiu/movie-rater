f4658a68c03c4d34cb683b527b296672555aaf01
jiangdongjiu <jiangdongjiu5@gmail.com>
2021-03-07 04:36:00 -0600

    hook context, pass values to components from inex.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,14 +6,21 @@ import Auth from './components/auth'
 import reportWebVitals from './reportWebVitals';
 import { Route, BrowserRouter } from "react-router-dom";
 
+export const TokenContext = createContext(null);
+
 function Router() {
+
+  const [token, setToken] = useState('');
+
   return (
-  <React.StrictMode>
-    <BrowserRouter>
-      <Route exact path="/" component={Auth} />
-      <Route exact path="/movies" component={App} />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <TokenContext.Provider value={{token, setToken}}>
+        <BrowserRouter>
+          <Route exact path="/" component={Auth} />
+          <Route exact path="/movies" component={App} />
+        </BrowserRouter>
+      </TokenContext.Provider>
+    </React.StrictMode>
   )
 }
 
